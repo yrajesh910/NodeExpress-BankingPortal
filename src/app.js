43766063c333c4d3c1697c1d569b98695fbbd5c2
Rajesh <rajesh.y@cognizant.com>
2019-05-31 23:30:39 +0530

    'commit'

--- src/app.js
+++ src/app.js
@@ -9,17 +9,39 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({extended: true}));
+
 const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
 const accounts = JSON.parse(accountData);
 
-const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
-const users = JSON.parse(userData);
+const usersData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
+const users = JSON.parse(usersData);
 
-app.get('/', (req, res) => res.render('index', {title: 'Account Summary', accounts: accounts}));
+app.get('/', (req, res) => res.render('index', {title: 'Account Summary', accounts}));
 
 app.get('/savings', (req,res) => res.render('account', {account: accounts.savings}));
 app.get('/checking', (req,res) => res.render('account', {account: accounts.checking}));
 app.get('/credit', (req,res) => res.render('account', {account: accounts.credit}));
 app.get('/profile', (req,res) => res.render('profile', {user: users[0]}));
+
+app.get('/transfer', (req, res) => res.render('transfer'));
+
+app.post('/transfer', (req, res) => {
+    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+    res.render('transfer', {message: 'Transfer completed'});
+});
+
+app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
+
+app.post('/payment', (req, res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+    res.render('payment', {message: 'Payment successfull', account: accounts.credit});
+})
 
 app.listen(3000, () => console.log('Server started on port 3000'));
